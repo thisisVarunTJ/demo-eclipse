680417ce3435a6b64521efc9038ac2ac4324752f
varuntjgithub@gmail.com <varuntjgithub@gmail.com>
2024-02-23 22:39:01 +0530

    commit to git

--- src/main/java/test/git/demo/Hello.java
+++ src/main/java/test/git/demo/Hello.java
@@ -2,8 +2,9 @@ package test.git.demo;
 
 public class Hello {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello git");
 
 	}
 
